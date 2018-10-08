e87a6526631e5dff1fd0a97edfdcbd66835817db
disman1229 <346025232@qq.com>
2018-10-08 21:18:22 +0800

    加班统计完成

--- myapp/adminx.py
+++ myapp/adminx.py
@@ -2,28 +2,48 @@
 # Register your models here.
 
 import xadmin
-from .models import Vmms
+from .models import Vmms,overTime
 from xadmin import views
+# 用户信息模块
+from django.contrib.auth.models import User
 
+# 车辆管理
 class VmmsAdmin(object):
-    list_display = ('id', 'carModel','useTest','testProgress', 'VIN','engineNumber', 'mileage', 'recipient', 'sampleDate', 'returnDate', 'transport', 'parkingLocation', 'remarks')
+    # 数据展示及顺序
+    list_display = ('carModel','engineDisplacement','gearBox', 'VIN','engineNumber', 'mileage', 'recipient', 'sampleDate', 'returnDate','useTest','testProgress', 'transport', 'parkingLocation', 'remarks')
+    # 搜索数据
     search_fields =('carModel','VIN')
+    # 数据筛选
     list_filter =('sampleDate', 'returnDate','testProgress',)
-
-
+    # 数据及时编辑
+    list_editable =('returnDate')
+    # 显示数据详细
+    show_detail_fields =('returnDate')
+    # 不可修改的数据
+    # readonly_fields =('VIN')
 
 xadmin.site.register(Vmms,VmmsAdmin)
 
+# 加班统计
+class overTimeAdmin(object):
+
+    list_display = ('overDate','startTime','endTime','duration','overTimeCause')
+    # list_filter = ('duration')
+
+
+xadmin.site.register(overTime,overTimeAdmin)
 
 # class BaseSetting(object):
 #     # 主题修改
 #     enable_themes = False
 #     use_bootswatch = False
 #
+
+#整体配置
 class GlobalSettings(object):
-    #整体配置
     site_title = "众泰试验室管理系统"
     site_footer = "Disman开发"
+    # base_template = 'xadmin/base_site.html'
     # menu_style = "accordion"    #菜单收起
 #
 # xadmin.site.register(views.BaseAdminView, BaseSetting)
--- myapp/models.py
+++ myapp/models.py
@@ -1,25 +1,15 @@
 from django.db import models
+import time
 
 from django.utils.html import format_html
 
 class Vmms(models.Model):
     # 车型
     carModel = models.CharField(max_length=255,verbose_name="车型")
-    # 试验用途
-    useTests = (
-        (0, "请选择试验用途"),
-        (1, "排放油耗试验"),
-        (2, "温度场试验"),
-        (3, "冷却试验"),
-        (4, "空调试验"),
-    )
-    useTest = models.IntegerField(choices=useTests, default=0,verbose_name="试验用途")
-    #试验进度
-    testProgresss = (
-        (0, "进行中"),
-        (1, "完成"),
-    )
-    testProgress = models.IntegerField(choices=testProgresss,default=0,verbose_name="试验进度")
+    # 发动机排量
+    engineDisplacement = models.CharField(max_length=10,verbose_name="发动机排量",blank=True)
+    #变速箱
+    gearBox = models.CharField(max_length=100,verbose_name="变速箱",blank=True)
     # VIN码
     VIN = models.CharField(max_length=17,verbose_name="VIN码")
     #发动机号
@@ -31,7 +21,22 @@ class Vmms(models.Model):
     # 接车时间
     sampleDate = models.DateTimeField(verbose_name="接车时间")
     # 还车时间
-    returnDate = models.DateTimeField(verbose_name="还车时间")
+    returnDate = models.DateTimeField(verbose_name="还车时间",blank=True)
+    # 试验用途
+    useTests = (
+        (0, "请选择试验用途"),
+        (1, "排放油耗试验"),
+        (2, "温度场试验"),
+        (3, "冷却试验"),
+        (4, "空调试验"),
+    )
+    useTest = models.IntegerField(choices=useTests, default=0, verbose_name="试验用途")
+    # 试验进度
+    testProgresss = (
+        (0, "进行中"),
+        (1, "完成"),
+    )
+    testProgress = models.IntegerField(choices=testProgresss, default=0, verbose_name="试验进度")
     # 运输
     transports = (
         (0,"不可拖运"),
@@ -46,7 +51,7 @@ class Vmms(models.Model):
     parkingLocation = models.IntegerField(choices=parkingLocations,default=0,verbose_name="停放地")
 
     # 备注
-    remarks = models.TextField(verbose_name="备注")
+    remarks = models.TextField(verbose_name="备注",blank=True)
 
     class Meta:
         db_table = 'vmms'
@@ -66,7 +71,60 @@ class Vmms(models.Model):
        else:
            color_code = 'red'
        return format_html(
-            '<span style="color: #{};">{} {}</span>',
+           '<span style="color: #{};">{} {}</span>',
            color_code,
            self.testProgress
        )
+
+
+# 加班统计模型
+class overTime(models.Model):
+    # 加班日期
+    overDate = models.DateField(verbose_name="加班日期")
+    # 开始时间
+    startTime = models.TimeField(verbose_name="开始时间")
+    #结束时间
+    endTime = models.TimeField(verbose_name="结束时间")
+    # 加班时长
+    # causeTime = models.IntegerField('causeTime1')
+    #加班事由（选填）
+    overTimeCause = models.CharField(max_length=255,verbose_name="加班事由")
+
+    class Meta:
+        db_table = 'overtime'
+        verbose_name = "加班统计"
+        verbose_name_plural = verbose_name
+
+    def duration(self):
+        # 计算时长1
+        # ehour = self.endTime.hour
+        # eminute = self.endTime.minute
+        # shour = self.startTime.hour
+        # sminute = self.startTime.minute
+        # chour = ehour - shour
+        # cminute = eminute - sminute
+
+        # 计算时长2
+        # print(self.endTime)
+        # endtime = time.mktime(self.endTime.timetuple())
+        # starttime = time.mktime(self.startTime.timetuple())
+        # print(endtime)
+        # dtime = endtime - starttime
+        # ctime = datetime.datetime.fromtimestamp(dtime)
+
+        # 计算时长3
+        sTime = str(self.startTime)
+        eTime = str(self.endTime)
+        def t2s(t):
+            h,m,s = t.strip().split(":")
+            return int(h) * 3600 + int(m) * 60 + int(s)
+        s1Time = t2s(sTime)
+        e1Time = t2s(eTime)
+        cTime = e1Time - s1Time
+        print(cTime)
+        dTime = time.strftime("%H:%M:%S", time.gmtime(cTime))
+        # print(c1Time)
+
+
+        return dTime
+    duration.short_description = "加班时长"
